1664fc9ec40e93d53cc3f1054a7902a155bb7924
miguelhttp <miguelalves1497@gmail.com>
2022-06-29 18:07:38 -0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReactDOM } from "react";
+import { useState } from "react";
+
+const App = () => {
+  const nome = 'Miguel'
+  return(
+    <div>
+      <h1>Hello,{nome}</h1>
+    </div>
+  )
+};
+
+export default App();
